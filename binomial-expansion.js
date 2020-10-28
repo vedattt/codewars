3e3f69da0e08614a0c6816c2b44da0e49328548a
vedattt <dev@vedat.xyz>
2020-10-28 21:39:25 +0300

    Solved binomial-expansion for Java and JS

---
+++ binomial-expansion.js
@@ -0,0 +1,102 @@
+const binomialCoefficient = (n, k) => {
+  const factorial = x => {
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+      result *= i;
+    }
+    return result;
+  };
+  
+  return factorial(n) / (factorial(k) * factorial(n-k));
+};
+
+function expand(expr) {
+  
+  // Disect expression into necessary variables
+  const matchedExpr = expr.match(/^\((-?\w+)\+?(-?\d+)\)\^(\d+)$/);
+  
+  let baseTerm1 = matchedExpr[1]; // Coefficient of the first term
+  let variable; // Variable of the first term
+  
+  // Separate variable from baseTerm1
+  if (baseTerm1[0] === "-" && baseTerm1.length === 2) { // If term is "-x"
+    variable = baseTerm1[1];
+    baseTerm1 = -1;
+  } else if (baseTerm1.length === 1 && !Number.isInteger(baseTerm1)) { // If term is "x"
+    variable = baseTerm1;
+    baseTerm1 = 1;
+  } else { // If term is anything else
+    variable = baseTerm1[baseTerm1.length - 1];
+    baseTerm1 = parseInt(baseTerm1.substring(0, baseTerm1.length - 1));
+  }
+  
+  let baseTerm2 = parseInt(matchedExpr[2]); // Coefficient of the second term (doesn't have a variable)
+  
+  const exponent = parseInt(matchedExpr[3]);
+  if (exponent === 0) return "1";
+  
+  let resultTerms = []; // Gather expanded terms in array
+  
+  // For every possible term...
+  for (let i = 0; i <= exponent; i++) {
+    let exponent1 = exponent - i;
+    let exponent2 = i;
+    
+    let coefficient = binomialCoefficient(exponent, exponent2);
+    
+    let term1 = Math.pow(baseTerm1, exponent1);
+    let term2 = Math.pow(baseTerm2, exponent2);
+    
+    let finalCoefficient = coefficient * term1 * term2;
+    
+    let finalTerm = "";
+    
+    // Form final version of current expanded term
+    if (finalCoefficient !== 0) { // No term will be formed if its coefficient is 0
+      
+      // Handle coefficients
+      if (finalCoefficient === 1) {
+        
+        // Do nothing yet so the term can be x instead of 1x
+        
+      } else if (finalCoefficient === -1) {
+        
+        finalTerm += "-"; // Make it so the term is -x instead of -1x
+        
+      } else { // If the term has a displayable coefficient
+        
+        finalTerm += finalCoefficient > 0 ? `+${finalCoefficient}` : finalCoefficient; // Add "+" sign to positive coefficients, otherwise just display the coefficient
+        if (exponent2 === 0 && finalCoefficient > 0) finalTerm = finalTerm.substring(1); // First term of expansion shouldn't have "+" sign
+        
+      }
+      
+      // Handle variables and exponents
+      if (exponent1 !== 0) { // If variable's exponent is a positive integer
+        
+        finalTerm += variable; // Display variable
+        finalTerm += exponent1 === 1 ? "" : `^${exponent1}`; // Unless the variable would be x^1, show its exponent
+        
+      } else { // If the variable's exponent is zero, the term will be a constant => no variable is added
+        
+        if (finalTerm === "-") { // If the term is -1, we can now show it as an integer
+          
+          finalTerm = "-1";
+          
+        } else if (finalTerm.length === 0 && finalCoefficient !== 0) { // If the term is +1, we can now show it as integer
+          
+          finalTerm = "+1";
+          if (exponent2 === 0) finalTerm = finalTerm.substring(1); // Don't show +1 if the term is the first term
+          
+        }
+        
+      }
+      
+      resultTerms.push(finalTerm);
+      
+    }
+    
+  }
+  
+  return resultTerms.join("");
+  
+}
